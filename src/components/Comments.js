be545608c5075465a56fa31ba32250c9e3223d8e
Guilherme Vierno <guilhermevierno@gmail.com>
2018-04-24 11:41:47 -0300

    Using comments from the server

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -3,37 +3,66 @@ import React from "react";
 export class Comment extends React.Component {
   state = {
     hidden: false
-  }
+  };
 
   toggle = () => {
     this.setState({
       hidden: !this.state.hidden
-    })
-  }
+    });
+  };
 
   render() {
     if (this.state.hidden) {
-      return <button onClick={this.toggle}>Mostrar comentário</button>
+      return <button onClick={this.toggle}>Mostrar comentário</button>;
     }
     return (
       <div className="App-comment">
         <p className="Comment-User">{this.props.user}</p>
-        <p className="Comment-Body">{this.props.content}</p>
+        {/* XSS WARNING!!! */}
+        <p
+          className="Comment-Body"
+          dangerouslySetInnerHTML={{__html: this.props.content.replace("\n", "<br>")}}
+        />
         <button onClick={this.toggle}>Esconder comentário</button>
       </div>
     );
   }
 }
 
-const Comments = () => {
-  return (
-    <div className="App-comments">
-      <h2>Comentários</h2>
-      <Comment user="João" content="Que matéria legal!" />
-      <Comment user="Maria" content="Que matéria chata!" />
-      <Comment user="José" content="Concordo com os dois!" />
-    </div>
-  );
-};
+// http://jsonplaceholder.typicode.com/comments?postId=1
+
+class Comments extends React.Component {
+  state = {
+    comments: []
+  };
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/comments?postId=1").then(
+      response => {
+        response.json().then(data => {
+          this.setState({
+            comments: data
+          });
+        });
+      }
+    );
+  }
+  render() {
+    return (
+      <div className="App-comments">
+        <h2>Comentários</h2>
+        {this.state.comments.map(comment => {
+          return (
+            <Comment
+              teste={comment}
+              key={comment.id}
+              user={comment.name}
+              content={comment.body}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default Comments;
