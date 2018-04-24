27aa0ddc32389402281c5df0a9d5090a6a82a8a8
Guilherme Vierno <guilhermevierno@gmail.com>
2018-04-24 01:15:01 -0300

    Moving article component

--- src/App.js
+++ src/App.js
@@ -2,44 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Comments from  "./components/Comments";
+import Article from  "./components/Article";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Lorem ipsum dolor sit amet</h1>
-        </header>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-          accusantium totam maiores eius, sed vel nemo esse consequatur veniam
-          aut voluptate cum asperiores dicta ipsum corporis! Odio earum fugit
-          excepturi.
-        </p>
-        <p>
-          Non repellendus et fugiat doloribus aliquid adipisci provident neque
-          tempore natus sit? Rerum pariatur voluptatibus, quidem minus
-          consectetur ab vel quos exercitationem corrupti facere neque ducimus.
-          Dignissimos laudantium ducimus ipsam.
-        </p>
-        <p>
-          Aliquam dignissimos id sunt quis neque autem, nam, labore eum animi
-          nihil aut? Repellat illum delectus totam nulla? Possimus quasi
-          molestias ullam totam omnis, perferendis modi cum similique. Esse,
-          dignissimos.
-        </p>
-        <p>
-          Minima dignissimos in hic accusamus, impedit voluptatibus molestias
-          quam autem quo, facilis provident! Repellat rerum iste, deleniti
-          maiores provident quaerat molestiae voluptatibus culpa suscipit
-          debitis quis ipsa consequatur ab sint!
-        </p>
-        <p>
-          Voluptatibus numquam velit esse explicabo culpa, deleniti nam ullam
-          dignissimos soluta maiores! Sequi accusantium consequatur numquam.
-          Veniam consequatur quisquam, atque quaerat expedita, eos possimus nisi
-          reprehenderit similique facilis architecto fugit?
-        </p>
+        <Article />
         <Comments />
       </div>
     );
