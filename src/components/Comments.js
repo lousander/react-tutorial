640105533ddc3a03afddb58cb7de92045260e950
Guilherme Vierno <guilhermevierno@gmail.com>
2018-04-24 01:19:41 -0300

    Using props for comment

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,21 +1,21 @@
 import React from "react"
 
+const Comment = props => {
+  return (
+    <div className="App-comment">
+      <p>{ props.user }</p>
+      <p>{ props.content }</p>
+    </div>
+  )
+}
+
 const Comments = () => {
   return (
     <div className="App-comments">
       <h2>Comentários</h2>
-      <div className="App-comment">
-        <p>João</p>
-        <p>Que matéria legal!</p>
-      </div>
-      <div className="App-comment">
-        <p>Maria</p>
-        <p>Que matéria chata!</p>
-      </div>
-      <div className="App-comment">
-        <p>José</p>
-        <p>Concordo com os dois!</p>
-      </div>
+      <Comment user="João" content="Que matéria legal!" />
+      <Comment user="Maria" content="Que matéria chata!" />
+      <Comment user="José" content="Concordo com os dois!" />
     </div>
   );
 };
