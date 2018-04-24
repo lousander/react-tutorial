2204ec79fd606d842c17d8b970ec8e9d81d801fa
Guilherme Vierno <guilhermevierno@gmail.com>
2018-04-24 11:33:46 -0300

    Fetching post data from jsonplaceholder

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,42 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Article = () => {
-  return (
-    <div>
-      <header className="App-header">
-        <h1 className="App-title">Lorem ipsum dolor sit amet</h1>
-      </header>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-        accusantium totam maiores eius, sed vel nemo esse consequatur veniam aut
-        voluptate cum asperiores dicta ipsum corporis! Odio earum fugit
-        excepturi.
-      </p>
-      <p>
-        Non repellendus et fugiat doloribus aliquid adipisci provident neque
-        tempore natus sit? Rerum pariatur voluptatibus, quidem minus consectetur
-        ab vel quos exercitationem corrupti facere neque ducimus. Dignissimos
-        laudantium ducimus ipsam.
-      </p>
-      <p>
-        Aliquam dignissimos id sunt quis neque autem, nam, labore eum animi
-        nihil aut? Repellat illum delectus totam nulla? Possimus quasi molestias
-        ullam totam omnis, perferendis modi cum similique. Esse, dignissimos.
-      </p>
-      <p>
-        Minima dignissimos in hic accusamus, impedit voluptatibus molestias quam
-        autem quo, facilis provident! Repellat rerum iste, deleniti maiores
-        provident quaerat molestiae voluptatibus culpa suscipit debitis quis
-        ipsa consequatur ab sint!
-      </p>
-      <p>
-        Voluptatibus numquam velit esse explicabo culpa, deleniti nam ullam
-        dignissimos soluta maiores! Sequi accusantium consequatur numquam.
-        Veniam consequatur quisquam, atque quaerat expedita, eos possimus nisi
-        reprehenderit similique facilis architecto fugit?
-      </p>
-    </div>
-  );
-};
+class Article extends Component {
+  state = {
+    title: "",
+    body: ""
+  }
+
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/posts/1/").then(response => {
+      response.json().then(data => {
+        this.setState({
+          title: data.title,
+          body: data.body,
+        })
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1 className="App-title">{ this.state.title }</h1>
+        </header>
+        {this.state.body.split("\n").map(block => {
+          return <p key={atob(block)}>{ block }</p>
+        })}
+      </div>
+    );
+  }
+}
 
 export default Article;
